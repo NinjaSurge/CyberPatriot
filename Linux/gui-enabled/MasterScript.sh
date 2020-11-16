928ac239c3e20ce635c11dc5b351cae1bad130df
NinjaSurge <rayktd@gmail.com>
2020-11-16 15:35:57 -0600

    gui-enabled editing

--- Linux/gui-enabled/MasterScript.sh
+++ Linux/gui-enabled/MasterScript.sh
@@ -154,36 +154,103 @@ removeUsers() {
 	      id -un "$user_" >> users_.txt
 	    fi
 	  done <users.txt
-	  cat ./users_.txt
 
-	  while IFS= read -r line; do
-			zenity --question --width=300 --title="Delete $line?" \
-				--text="Do you want to remove $line and all their files?"
-	    if [ $? -eq 0 ]; then
-				echo $sudoPassword | sudo -S userdel -r "$line" | tee >(zenity --width=200 --height=100 \
-		    	 --title="Collecting Information" --progress \
-		    	 --pulsate --text="Upgrading computer..." \
-		    	 --auto-kill --auto-close)
-				if [ $? -eq 0 ]; then
-					zenity --info --title="Removed Sucessfully" --text="$line removed sucessfully."
-				else
-					zenity --error --title="Removal Failed" --text="Removing $line failed."
-				fi
-	    fi
-	  done <users_.txt
+		OptionsArray=()
+		while IFS= read -r line; do
+			OptionsArray+=( "FALSE $line" )
+		done <users_.txt
+		checkUsers=$(zenity --list --text "How linux.byexamples can be improved?" --checklist  --column "Pick" \
+		--column "options" ${OptionsArray[@]} \
+		--separator=":")
+
+		echo $checkUsers > checkUsers | sed 's/:/\n/g' ./checkUsers > NewCheckUsers
+
+		cat NewCheckUsers > ./checkUsers
+		cat ./checkUsers
+
+		while IFS= read -r line; do
+			echo $sudoPassword | sudo -S userdel -r $line
+		done < ./checkUsers
+
 		rm ./users_.txt
 		rm ./users.txt
+		rm ./NewCheckUsers
+		rm ./checkUsers
 	else
 		zenity --info --width=200 --title="Continue" \
 			--text="You choose not to remove any users"
 	fi
 }
 
+add_Users() {
+	zenity --question --width=300 --title="Add Users" \
+		--text="Do you want to add any users?"
+	if [ $? -eq 0 ]; then
+		loop="Yes"
+		while [ "$loop" == "Yes" ]; do
+			zenity --forms --title="Add Friend" \
+				--text="Enter information about your friend." \
+				--separator=":" \
+				--add-entry="Full Name" \
+			  --add-entry="Username" \
+			  --add-password="Password" > TempNewUserList \
+				--add-entry="Group"
+
+			# seperates the : into new lines for easier parsing
+			sed 's/:/\n/g' ./TempNewUserList > ./NewUserList
+
+			# removes the now useless file
+			rm ./TempNewUserList
+
+			# assigns the name variable to the specified name
+			name=$(sed -n '1p' ./NewUserList)
+
+			# assigns the username variable to the specified username
+			username=$(sed -n '2p' ./NewUserList)
+
+			# assigns the password variable to the specified password
+			password=$(sed -n '3p' ./NewUserList)
+
+			# adds the user
+			echo $sudoPassword | sudo -S useradd -m $username
+
+			# gives the user the specified password
+			echo "$username:$password" | sudo chpasswd
+
+			# deletes all entries but the group entries
+			sed -i '1,3d' ./NewUserList
+
+			# loops through the specified groups
+			while IFS= read -r line; do
+				# adds the user to the specified group
+				sudo usermod -aG  $line $username
+			# end of groups loop
+			done < NewUserList
+
+			zenity --info --title="Done" --text="User added"
+
+			# asking if the user wants to end the loop
+			loop=`zenity --title="Add More Users?" \
+				--list --radiolist --text="Would you like to add another user?" \
+				--column 'Selection' \
+				--column 'Answer' TRUE "Yes" FALSE "No"`
+		done
+		# clean up
+		rm ./NewUserList
+	else
+		zenity --info --width=200 --title="Continue" \
+			--text="You choose not to add any users"
+	fi
+}
+
+
 if [ $? -eq 0 ]; then
 	updateMachine
 	searchFiles
 	enableFirewall
+	add_Users
 	removeUsers
+
 
 	zenity --info --width=300 --title="Script End" \
 		--text="This is the end of the script. Hope it helped!"
