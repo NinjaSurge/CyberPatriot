b80978670f98cb4acdb604e250c69667b9123b53
NinjaSurge <rayktd@gmail.com>
2020-12-01 10:59:36 -0600

    Made the Start.sh file work like its supposed to, and added a test script functionality

--- Linux/Start.sh
+++ Linux/Start.sh
@@ -3,8 +3,11 @@ echo "Do you want a gui for your script?"
 echo "(!!Is currently Experimental and not as complete as the nogui version!!)"
 echo "Type \"yes\" for the gui"
 read answer
+if [ "$1" == "test" ]; then
+  bash ./files/testScript.sh $2
+fi
 if [ $answer = "yes" ]; then
-  bash ./gui/MasterScript.sh $1
+  bash ./files/gui/MasterScript.sh $1
 else
-  bash ./nogui/MasterScript.sh $1
+  bash ./files/nogui/MasterScript.sh $1
 fi
